473f3805ba26722bdce594bd479131ef7fe854a3
Manirohith08 <135613002+Manirohith08@users.noreply.github.com>
2025-11-19 10:31:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-from langchain.document_loaders import TextLoader
-from langchain.text_splitters import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from transformers import pipeline
